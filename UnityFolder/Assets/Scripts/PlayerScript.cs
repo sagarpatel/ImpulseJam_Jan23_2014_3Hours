69933d69e7a2672a36265af1f11e538f45c2f9f4
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-23 16:14:44 +0900

    Basic player prefab and controls

---
+++ UnityFolder/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour 
+{
+
+	public float hSpeedScale = 1.0f;
+	public float vSpeedScale = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		HandleMovement();
+	
+	}
+
+	void HandleMovement()
+	{
+		Vector3 tempPosition = transform.position;
+		tempPosition.x +=  Input.GetAxis("Horizontal") * hSpeedScale * Time.deltaTime;
+		tempPosition.y +=  Input.GetAxis("Vertical") * vSpeedScale * Time.deltaTime;
+		transform.position = tempPosition;
+	}
+
+}
