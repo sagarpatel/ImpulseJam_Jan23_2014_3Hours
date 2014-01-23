6be46537fa033983adbad7b30256219d03fe8b48
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-23 17:27:08 +0900

    Basic enemy prefab and bullet collision working

---
+++ UnityFolder/Assets/Scripts/BasicEnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+        if (coll.gameObject.tag == "Bullet")
+        {
+        	Destroy(coll.gameObject, 0.0f); // destroy bullet
+        	Destroy(gameObject, 0.9f); // self destruct
+        }
+
+    }
+}
