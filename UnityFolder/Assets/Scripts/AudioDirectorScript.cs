ad7150db204ea77241b3731dcd741bd612d83432
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-23 18:27:21 +0900

    Implemented device selction

--- UnityFolder/Assets/Scripts/AudioDirectorScript.cs
+++ UnityFolder/Assets/Scripts/AudioDirectorScript.cs
@@ -6,6 +6,7 @@ public class AudioDirectorScript : MonoBehaviour
 	public bool requestPending;
 	public bool isAuthget = false;
 	public string[] devicesArray;
+	public int currentlySelectedDeviceIndex = 0;
 
 	// Use this for initialization
 	void Start () 
@@ -20,7 +21,16 @@ public class AudioDirectorScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if(Input.GetKeyUp("e") || Input.GetKeyUp("e") )
+		{
+			if(isAuthget)
+			{
+				if(currentlySelectedDeviceIndex >= devicesArray.Length -1)
+					currentlySelectedDeviceIndex = 0;
+				else
+					currentlySelectedDeviceIndex += 1;
+			}
+		}
 	}
 
 	public IEnumerator RequestAuthorize()
@@ -47,14 +57,27 @@ public class AudioDirectorScript : MonoBehaviour
 
 	void OnGUI()
 	{
-    	//GUI.Label(Rect(0,0,Screen.width,Screen.height),"TEST");
-		float yPos = 0;
-    	float offset = 10.0f;
-		foreach(string device in devicesArray)
-    	{
-    		GUI.Label(new Rect(10, yPos, 100, 20), device);
-    		yPos += offset;
-    	}
+		if(isAuthget)
+		{
+	    	GUI.Label(new Rect(0,0,200,20),"List of available devices:");
+	    	GUI.Label(new Rect(200,0,200,20),"Press e to switch devices");
+
+			float yPos = 0;
+	    	float offset = 10.0f;
+			foreach(string device in devicesArray)
+	    	{
+	    		GUI.Label(new Rect(10, 20 + yPos, 500, 20), device);
+	    		yPos += offset;
+	    	}
+
+			GUI.Label(new Rect(0, 40 + yPos,200,20),"Currently selected device:");
+			GUI.Label(new Rect(10, 60 + yPos,500,20), devicesArray[currentlySelectedDeviceIndex]);
+		}
+		else
+		{
+			GUI.Label(new Rect(0,0,1000,20),"FAILED TO GET AUTHENTIFICATION FOR MICROPHONE INPUT");			
+		}
+
 	}
 
 }
