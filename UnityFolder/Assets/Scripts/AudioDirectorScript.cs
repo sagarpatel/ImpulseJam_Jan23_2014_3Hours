03076cd670323bb8e20397e7ca09f35e64873bd2
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-23 18:09:59 +0900

    Debug display devices

--- UnityFolder/Assets/Scripts/AudioDirectorScript.cs
+++ UnityFolder/Assets/Scripts/AudioDirectorScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class AudioDirectorScript : MonoBehaviour 
 {
 	public bool requestPending;
+	public bool isAuthget = false;
+	public string[] devicesArray;
 
 	// Use this for initialization
 	void Start () 
@@ -28,14 +30,31 @@ public class AudioDirectorScript : MonoBehaviour
 		if (Application.HasUserAuthorization(UserAuthorization.Microphone))
 		{	
 			Debug.Log("MICROPHONE AUTH GET!");
+			devicesArray = Microphone.devices;	
+			isAuthget = true;
 			//InitMicrophone();
 		}
 
-		Debug.Log(Microphone.devices.Length);	
+		Debug.Log(Microphone.devices.Length);
+		
 		foreach (string device in Microphone.devices) 
 		{
             Debug.Log("Name: " + device);
         }
+	}
+
+
+
+	void OnGUI()
+	{
+    	//GUI.Label(Rect(0,0,Screen.width,Screen.height),"TEST");
+		float yPos = 0;
+    	float offset = 10.0f;
+		foreach(string device in devicesArray)
+    	{
+    		GUI.Label(new Rect(10, yPos, 100, 20), device);
+    		yPos += offset;
+    	}
 	}
 
 }
