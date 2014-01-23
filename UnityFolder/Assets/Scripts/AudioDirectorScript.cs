3a96b6df5c836c2b358e2f5443e4e82b2b56de21
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-23 17:49:13 +0900

    Got web auth request working for mic input

---
+++ UnityFolder/Assets/Scripts/AudioDirectorScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioDirectorScript : MonoBehaviour 
+{
+	public bool requestPending;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log("AUDIODIRECTOR START");
+
+		StartCoroutine("RequestAuthorize");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public IEnumerator RequestAuthorize()
+	{
+		requestPending = true;
+		yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+		if (Application.HasUserAuthorization(UserAuthorization.Microphone))
+		{	
+			Debug.Log("MICROPHONE AUTH GET!");
+			//InitMicrophone();
+		}
+
+		Debug.Log(Microphone.devices.Length);	
+		foreach (string device in Microphone.devices) 
+		{
+            Debug.Log("Name: " + device);
+        }
+	}
+
+}
