5391a200b9b19da0d5cab6fcae66cb992ab0eccb
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-24 19:33:17 +0900

    Weapons array rigged up, reacting to music.

---
+++ UnityFolder/Assets/Scripts/WeaponsArrayScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponsArrayScript : MonoBehaviour 
+{
+
+	GameObject audioDirector;
+	public Color calculatedRGB;
+
+	public GameObject lowBulletPrefab;
+	public GameObject midBulletPrefab;
+	public GameObject highBulletPrefab;
+
+	GameObject lowWeapon;
+	GameObject midWeapon;
+	GameObject highWeapon;
+
+	public float weaponsScale = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		audioDirector = GameObject.FindWithTag("AudioDirector");	
+
+		lowWeapon = (GameObject)Instantiate(lowBulletPrefab, new Vector3(), Quaternion.identity);
+		midWeapon = (GameObject)Instantiate(midBulletPrefab, new Vector3(), Quaternion.identity);
+		highWeapon = (GameObject)Instantiate(highBulletPrefab, new Vector3(), Quaternion.identity);
+
+		lowWeapon.transform.parent = transform;
+		midWeapon.transform.parent = transform;
+		highWeapon.transform.parent = transform;
+
+		lowWeapon.transform.localPosition = new Vector3(-3, 0, 0);
+		midWeapon.transform.localPosition = new Vector3(0, 0, 0);
+		highWeapon.transform.localPosition = new Vector3(3, 0, 0);
+
+		// deactivate the bullet script on weapons so that they don't fly away
+		lowWeapon.GetComponent<BasicBulletScript>().enabled = false;
+		midWeapon.GetComponent<BasicBulletScript>().enabled = false;
+		highWeapon.GetComponent<BasicBulletScript>().enabled = false;
+
+		lowWeapon.tag = "Weapon";
+		midWeapon.tag = "Weapon";
+		highWeapon.tag = "Weapon";
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		calculatedRGB = audioDirector.GetComponent<AudioDirectorScript>().calculatedRGB;
+		HandleWeaponScales();
+	
+	}
+
+	void HandleWeaponScales()
+	{
+
+		lowWeapon.transform.localScale = calculatedRGB.r * weaponsScale * lowBulletPrefab.transform.localScale;
+		midWeapon.transform.localScale = calculatedRGB.g * weaponsScale * midBulletPrefab.transform.localScale;
+		highWeapon.transform.localScale = calculatedRGB.b * weaponsScale *  highBulletPrefab.transform.localScale;
+
+	}
+
+}
